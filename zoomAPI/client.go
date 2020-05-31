51824f700084954d106548e4a4beb6cfa206f49e
Melvin Vivas <melvindave@gmail.com>
2020-05-31 16:05:46 +0800

    return error when api call response codes are not 200, 201 or 204. also distinguish unauthorized 401 response.

--- zoomAPI/client.go
+++ zoomAPI/client.go
@@ -2,8 +2,10 @@ package zoomAPI
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -20,7 +22,7 @@ func NewClient(apiUrl string, authToken string) (client Client) {
 	return
 }
 
-func (client Client) executeRequest(endpoint string, httpMethod string) (response []byte, err error){
+func (client Client) executeRequest(endpoint string, httpMethod string) (response []byte, err error) {
 	httpClient := &http.Client{}
 
 	url := fmt.Sprintf("%s%s", client.url, endpoint)
@@ -29,12 +31,27 @@ func (client Client) executeRequest(endpoint string, httpMethod string) (respons
 		return
 	}
 
-	req.Header.Add("Authorization", "Bearer " + client.authToken)
+	req.Header.Add("Authorization", "Bearer "+client.authToken)
 	req.Header.Add("Content-Type", "application/json")
 
 	var resp *http.Response
 	resp, err = httpClient.Do(req)
 	if err != nil {
+		return
+	}
+
+	httpStatusCode := resp.StatusCode
+	log.Printf("http response statusCode = %d", httpStatusCode)
+
+	if httpStatusCode != 200 {
+		switch httpStatusCode {
+		case 401:
+			err = errors.New(fmt.Sprintf("unauthorized error %d encountered, " +
+				"check your auth token if it's still valid", httpStatusCode))
+		default:
+			err = errors.New(fmt.Sprintf("http error %d encountered in API call", httpStatusCode))
+		}
+
 		return
 	}
 
@@ -46,8 +63,7 @@ func (client Client) executeRequest(endpoint string, httpMethod string) (respons
 	return
 }
 
-
-func (client Client) executeRequestWithBody(endpoint string, httpMethod string, body []byte) (response []byte, err error){
+func (client Client) executeRequestWithBody(endpoint string, httpMethod string, body []byte) (response []byte, err error) {
 	httpClient := &http.Client{}
 
 	url := fmt.Sprintf("%s%s", client.url, endpoint)
@@ -67,6 +83,22 @@ func (client Client) executeRequestWithBody(endpoint string, httpMethod string, 
 		return
 	}
 
+	httpStatusCode := resp.StatusCode
+	log.Printf("http response statusCode = %d", httpStatusCode)
+
+	if httpStatusCode != 200 && httpStatusCode != 201 && httpStatusCode != 204 {
+
+		switch httpStatusCode {
+		case 401:
+			err = errors.New(fmt.Sprintf("unauthorized error %d encountered, " +
+				"check your auth token if it's still valid", httpStatusCode))
+		default:
+			err = errors.New(fmt.Sprintf("http error %d encountered in API call", httpStatusCode))
+		}
+
+		return
+	}
+
 	response, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return
@@ -74,6 +106,3 @@ func (client Client) executeRequestWithBody(endpoint string, httpMethod string, 
 
 	return
 }
-
-
-
